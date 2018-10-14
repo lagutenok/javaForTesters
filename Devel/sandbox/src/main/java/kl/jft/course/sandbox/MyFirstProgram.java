5346b04d52a45ed001909cc60e83d3b355f4e1d9
Lagutenok <lagutenok.ekaterina@gmail.com>
2018-10-14 19:47:12 +0200

    Added some functions

--- Devel/sandbox/src/main/java/kl/jft/course/sandbox/MyFirstProgram.java
+++ Devel/sandbox/src/main/java/kl/jft/course/sandbox/MyFirstProgram.java
@@ -2,6 +2,27 @@ package kl.jft.course.sandbox;
 
 public class MyFirstProgram {
    public static void main(String[] args) {
-      System.out.println("Hello, world!");
+      hello("World");
+      hello("user");
+      hello("Kate");
+
+      double l = 5;
+      System.out.println("the area of a square with side " + l + " = " + area(l));
+
+      double a = 5;
+      double b = 6;
+      System.out.println("the area of a rectangle with sides " + a + " and " + b + " = " + area(a, b));
+   }
+   public static void hello(String somebody){
+
+      System.out.println("Hello, " + somebody + "!");
+   }
+
+   public static double area(double len){
+      return len * len;
+   }
+
+   public static double area(double a, double b){
+      return a * b;
    }
 }
