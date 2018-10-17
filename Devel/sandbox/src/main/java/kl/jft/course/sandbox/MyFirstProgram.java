3faa83b6b7d430cff692b33d0207c0d3fa1e863a
Lagutenok <lagutenok.ekaterina@gmail.com>
2018-10-17 07:51:34 +0200

    Added 2 Classes: Square and Rectangle

--- Devel/sandbox/src/main/java/kl/jft/course/sandbox/MyFirstProgram.java
+++ Devel/sandbox/src/main/java/kl/jft/course/sandbox/MyFirstProgram.java
@@ -1,32 +1,32 @@
 package kl.jft.course.sandbox;
 
 public class MyFirstProgram {
-   public static void main(String[] args)
-   {
+   public static void main(String[] args) {
       hello("World");
       hello("user");
       hello("Kate");
 
-      double l = 5;
-      System.out.println("the area of a square with side " + l + " = " + area(l));
 
-      double a = 5;
-      double b = 6;
-      System.out.println("the area of a rectangle with sides " + a + " and " + b + " = " + area(a, b));
+      Square s = new Square(5);
+
+      System.out.println("the area of a square with side " + s.l + " = " + area(s));
+
+      Rectangle rec = new Rectangle(5, 6);
+
+      System.out.println("the area of a rectangle with sides " + rec.a + " and " + rec.b + " = " + area(rec));
    }
 
-   public static void hello(String somebody)
-   {
+   public static void hello(String somebody) {
       System.out.println("Hello, " + somebody + "!");
    }
 
-   public static double area(double len)
-   {
-      return len * len;
+   public static double area(Square s) {
+      return s.l * s.l;
    }
 
-   public static double area(double a, double b)
+
+   public static double area(Rectangle rec)
    {
-      return a * b;
+      return rec.a * rec.b;
    }
 }
---
+++ Devel/sandbox/src/main/java/kl/jft/course/sandbox/Square.java
@@ -0,0 +1,11 @@
+package kl.jft.course.sandbox;
+
+public class Square {
+
+   public double l;
+
+   public Square (double l) {
+      this.l = l;
+   }
+
+}
