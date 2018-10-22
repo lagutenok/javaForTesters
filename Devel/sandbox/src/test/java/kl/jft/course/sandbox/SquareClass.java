b2f3cd3dd2b0c4b9e05661aaac924c86396a9a94
Lagutenok <lagutenok.ekaterina@gmail.com>
2018-10-22 07:42:26 +0200

    Fix first test lesson

--- Devel/sandbox/src/test/java/kl/jft/course/sandbox/SquareClass.java
+++ Devel/sandbox/src/test/java/kl/jft/course/sandbox/SquareClass.java
@@ -1,12 +1,13 @@
 package kl.jft.course.sandbox;
 
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareClass {
    @Test
    public void testArea() {
       Square s = new Square(5);
-      assert s.area() == 25;
-
+      Assert.assertEquals(s.area(), 25.0);
    }
 }
